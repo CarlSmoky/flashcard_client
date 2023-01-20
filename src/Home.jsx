4c645590d425885d45ab6fc62ae8d321575abcf2
tsumitakaoru <k.tsumita01@gmail.com>
2023-01-20 15:49:57 -0500

    Change Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
-import Footer from './Footer'
 
 const Home = () => {
   return (
     <>
       <Wrapper>
-        <div className="left">left</div>
-        <div className="center">
           <div className="header">
             <h1>Memorize anything with free digital flashcards</h1>
             <p>Lorem Ipsum is simply dummy text of the
               printingand typesetting industry. </p>
           </div>
-          <div className="contents">
-            <div className="card">
+          <div className="cards">
+            <div className="card-1">
               <h2>Search Deck</h2>
               <p></p>
             </div>
-            <div className="card">
+            <div className="card-2">
               <h2>Create Deck</h2>
               <p></p>
             </div>
-            <div className="card">
+            <div className="card-3">
               <h2>Practice</h2>
               <p></p>
             </div>
           </div>
-        </div>
-        <div className="right">right</div>
       </ Wrapper>
     </>
   )
@@ -36,33 +31,46 @@ const Home = () => {
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
-  width: 100vw;
+  width: calc(100vw - 9.3rem - 9.3rem);
+  height: 65rem;
   
-  .left {
-    min-width: 8.5rem;
-    height: 100vh;
-    border-right: .4rem solid var(--black-primary);
-    border-left: .4rem solid var(--black-primary);
+  .header {
+    height: 10rem;
+    padding: 10rem;
+    h1 {
+      font-size: 2rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: center;
+    }
   }
 
-  .right {
-    min-width: 8.5rem;
-    height: 100vh;
-    border-right: .4rem solid var(--black-primary);
-    border-left: .4rem solid var(--black-primary);
-  }
-
-  .contents {
+  .cards {
     display: flex;
     flex-direction: row;
     justify-content: center;
-    min-width: 100rem;
-    .card {
-      width: 30%;
+    width: calc(100vw - 9.3rem - 9.3rem);
+    .card-1 {
+      width: 34%;
+      height: 30rem;
+      border-top: .3rem solid var(--black-primary);
+      border-bottom: .3rem solid var(--black-primary);
+    }
+    .card-2 {
+      width: 33%;
       height: 30rem;
       border: .3rem solid var(--black-primary);
+    }
+    .card-3 {
+      width: 34%;
+      height: 30rem;
+      border-top: .3rem solid var(--black-primary);
+      border-bottom: .3rem solid var(--black-primary);
     }
   }
 `
