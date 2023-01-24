8c450532eec8f7a95a1683320e8689e4fc86e494
tsumitakaoru <k.tsumita01@gmail.com>
2023-01-24 12:32:23 -0500

    Change component accoding to changing file name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Grid from '../components/Grid'
+import Frame from '../components/Frame'
 
 const Home = ({content}) => {
   
   return (
-    <Grid content={content}/>
+    <Frame content={content}/>
   )
 }
 
