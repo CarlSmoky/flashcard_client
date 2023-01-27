aa15cc03ae614953039922dd251074666cd6b5fb
tsumitakaoru <k.tsumita01@gmail.com>
2023-01-27 18:32:04 -0500

    Add src/components/DeckItem.jsx

---
+++ src/components/DeckItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DeckItem = () => {
+  return (
+    <Card>DeckItem</Card>
+  )
+}
+
+const Card = styled.div`
+`
+
+export default DeckItem
