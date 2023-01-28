2d7460bb2f437b72833edc0ea469b2bb98be9b6c
tsumitakaoru <k.tsumita01@gmail.com>
2023-01-28 17:56:46 -0500

    Add src/components/Decks.jsx

--- src/components/Decks.jsx
+++ src/components/Decks.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
+import axios from "axios";
+import DeckItem from './DeckItem';
 
 const Decks = () => {
+
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    
+    axios.get(`api/deck/`)
+    .then(res => {
+      const allDecks = res.data;
+      setDecks(allDecks);
+    })
+    .catch (err => {
+      console.log(err)
+    })
+  }, []);
+  
+  const allDecks = decks.map(deck => {
+    return (
+      <DeckItem
+        key={deck.id}
+        id={deck.id}
+        deckName={deck.deck_name}
+        user_id={deck.user_id}
+      />
+    )
+  });
+    
   return (
     <Wrapper>
-      <h1>decks</h1>
+      {allDecks}
     </Wrapper>
   )
 }
