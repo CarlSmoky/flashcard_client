07066cbebd27fe0c8340add7b3022a820142d541
tsumitakaoru <k.tsumita01@gmail.com>
2023-02-01 18:27:31 -0500

    Refactor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import DeckSettings from './DeckSettings';
+import axios from 'axios'
+import DeckSettings from './DeckSettings'
 import { useParams } from 'react-router-dom'
 
 const Card = () => {
-  const { deckName } = useParams();
+  const { id } = useParams();
   const [numCard, setNumCard] = useState(0);
+
+  const [cards, setCards] = useState([]);
+  
+
+  useEffect(() => {
+    axios.get(`api/card/deck/${id}`)
+    .then(res => {
+      const allCardsById = res.data;
+      setCards(allCardsById);
+    })
+    .catch (err => {
+      console.log(err)
+    })
+  }, [numCard]);
+
+
+  
+  // const allDecks = decks.map(deck => {
+  //   return (
+  //     <DeckItem
+  //       key={deck.id}
+  //       id={deck.id}
+  //       deckName={deck.deck_name}
+  //       user_id={deck.user_id}
+  //     />
+  //   )
+  // });
 
   return (
     <>
-    {numCard === 0 && <DeckSettings setNumCard={setNumCard} deckName={deckName}/>}
+    {numCard === 0 && <DeckSettings setNumCard={setNumCard} id={id}/>}
     <StyledCard>
       Card
     </StyledCard>
