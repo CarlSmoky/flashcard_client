717017e3b4c393af38bac796dc6c93c19818769e
tsumitakaoru <k.tsumita01@gmail.com>
2023-02-02 11:10:28 -0500

    Add Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,55 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import axios from 'axios'
-import DeckSettings from './DeckSettings'
-import { useParams } from 'react-router-dom'
+import React from 'react'
 
 const Card = () => {
-  const { id } = useParams();
-  const [numCard, setNumCard] = useState(0);
-
-  const [cards, setCards] = useState([]);
-  
-
-  useEffect(() => {
-    axios.get(`api/card/deck/${id}`)
-    .then(res => {
-      const allCardsById = res.data;
-      setCards(allCardsById);
-    })
-    .catch (err => {
-      console.log(err)
-    })
-  }, [numCard]);
-
-
-  
-  // const allDecks = decks.map(deck => {
-  //   return (
-  //     <DeckItem
-  //       key={deck.id}
-  //       id={deck.id}
-  //       deckName={deck.deck_name}
-  //       user_id={deck.user_id}
-  //     />
-  //   )
-  // });
-
   return (
-    <>
-    {numCard === 0 && <DeckSettings setNumCard={setNumCard} id={id}/>}
-    <StyledCard>
-      Card
-    </StyledCard>
-    </>
+    <div>Card</div>
   )
 }
-
-const StyledCard = styled.div`
-  width: 100%;
-  height: 40rem;
-  background: var(--quaternary-color);
-  z-index: -1;
-`
 
 export default Card
