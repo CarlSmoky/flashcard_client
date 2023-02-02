ae00ea476f4896aa89c258be0f8efcb66570d244
tsumitakaoru <k.tsumita01@gmail.com>
2023-02-02 12:39:48 -0500

    Add front and back card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react"
+import styled from "styled-components"
 
-const Card = () => {
+export default function Card({ card }) {
+  const [side, setSide] = useState();
+
+  function handleClick() {
+    console.log("clicked!");
+    setSide(!side);
+    console.log(side);
+  }
+
   return (
-    <div>Card</div>
-  )
+
+    <StyledCard className={`card ${side ? "side" : ""}`} onClick={handleClick}>
+      <small>
+        <span>Card ID</span>
+        {card.id}
+      </small>
+      {/* {side ? card.fields.side1 : card.fields.side2} */}
+      <div className="front">{card.term}</div>
+      <div className="back">{card.definition}</div>
+    </StyledCard>
+  );
 }
 
-export default Card
+const StyledCard = styled.div`
+  width: 100%;
+  height: 20rem;
+  border: var(--side-column-border) solid var(--black-primary);
+
+  
+`
