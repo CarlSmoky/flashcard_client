657fb7e7de76bfdbc02d2196f7268f82d0cca90a
tsumitakaoru <k.tsumita01@gmail.com>
2023-02-03 18:07:17 -0500

    Add Style for cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,7 +9,32 @@ const Cards = () => {
   const { id } = useParams();
   const [numCard, setNumCard] = useState(0);
 
+  const [deck, setDeck] = useState("");
+  const {deck_name} = deck;
+
   const [flashcarddata, setFlashcarddata] = useState([]);
+
+  useEffect(() => {
+    axios.get(`api/deck/${id}`)
+    .then(res => {
+      const deckById = res.data;
+      setDeck(deckById);
+    })
+    .catch (err => {
+      console.log(err)
+    })
+  }, []);
+
+  useEffect(() => {
+    axios.get(`api/card/deck/${id}`)
+    .then(res => {
+      const flashcardDataByDeckId = res.data;
+      setFlashcarddata(flashcardDataByDeckId);
+    })
+    .catch (err => {
+      console.log(err)
+    })
+  }, [numCard]);
 
   const cards = flashcarddata.map((card) => {
     return <Card card={card} key={card.id} />;
@@ -26,27 +51,18 @@ const Cards = () => {
     setCurrent(current + 1);
   }
   
-
-  useEffect(() => {
-    axios.get(`api/card/deck/${id}`)
-    .then(res => {
-      const flashcardDataByDeckId = res.data;
-      setFlashcarddata(flashcardDataByDeckId);
-    })
-    .catch (err => {
-      console.log(err)
-    })
-  }, [numCard]);
-
   return (
     <>
-    {numCard === 0 && <DeckSettings setNumCard={setNumCard} id={id}/>}
+    {numCard === 0 && <DeckSettings setNumCard={setNumCard} deckName={deck_name} id={id}/>}
     <Wrapper>
       {/* number of cards */}
-      {numCard > 0 && flashcarddata && flashcarddata.length > 0 ? (
-        <div className="cardNumber">
-          Card {current + 1} of {flashcarddata.length}
-        </div>
+      {flashcarddata && flashcarddata.length > 0 ? (
+        <>
+        <Header>{deck_name}</Header>
+        <CardCount>
+          {current + 1} / {flashcarddata.length}
+        </CardCount>
+        </>
       ) : (
         ""
       )}
@@ -56,7 +72,7 @@ const Cards = () => {
       {flashcarddata && flashcarddata.length > 0 ? cards[current] : loading}
       {/* /render cards */}
       {/* render nav buttons */}
-      <div className="nav">
+      <ButtonWrapper>
         {current > 0 ? (
           <button onClick={previousCard}>Previous card</button>
         ) : (
@@ -72,17 +88,37 @@ const Cards = () => {
           </button>
         )}
         {/* /render nav buttons */}
-      </div>
+      </ButtonWrapper>
     </Wrapper>
     </>
   )
 }
 
-const Wrapper = styled.div`
+const Wrapper  = styled.div`
   width: 100%;
-  height: 40rem;
-  background: var(--quaternary-color);
+  height: 50rem;
+  background: var(--white);
   z-index: -1;
+`
+
+const Header = styled.h4`
+  font-size: 2rem;
+  text-align: left;
+  padding: 3rem 0 0 1rem;
+`
+
+const CardCount = styled.h4`
+  font-size: 1.5rem;
+  padding-top: 1rem;
+`
+const ButtonWrapper = styled.div`
+  margin: 2rem 0;
+
+  button {
+    margin: 1rem;
+    width: 38%;
+    height: 3rem;
+  }
 `
 
 export default Cards
