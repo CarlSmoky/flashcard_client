ec8007ef58e70d8336a9473e8d564582b6e6c6b7
tsumitakaoru <k.tsumita01@gmail.com>
2023-03-06 20:05:06 -0500

    Add Result.jsx

---
+++ src/components/Result.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Result = () => {
+  return (
+    <Wrapper>Result</Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+
+`
+
+export default Result
