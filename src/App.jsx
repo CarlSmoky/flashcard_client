76e0fabab8103fb3086353305a55d27c1c1c7388
tsumitakaoru <k.tsumita01@gmail.com>
2023-03-06 20:03:37 -0500

    Add Result and route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path ="/decklist" element={<Frame content='decklist'/>}/>
         {/* <Route path ="/search" element={<Search />}/> */}
         <Route path ="/deck/:id" element={<Frame content='cards'/>}/>
+        <Route path ="/result" element={<Frame content='result'/>}/>
       </Routes>
       </BrowserRouter>
     </div>
