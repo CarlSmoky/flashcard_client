22c8bfed0712cd1f208ebd27684b3197583b7184
tsumitakaoru <k.tsumita01@gmail.com>
2023-03-08 10:10:40 -0500

    Fix cross button disable

--- src/components/CardsHeader.jsx
+++ src/components/CardsHeader.jsx
@@ -76,8 +76,6 @@ const Button = styled.button`
     svg {
     font-size: 2.5rem;
     padding: .2rem;
-    border-radius: 50%;
-    transition: all .3s;
     stroke-width: 1.2;
     }
 
@@ -88,31 +86,8 @@ const Button = styled.button`
       `
       : css`
       cursor: pointer;
-      &::after {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 4px;
-        transform: scaleX(0);
-        bottom: 0;
-        left: 0;
-        background-color: var(--primary-color);
-        transform-origin: bottom right;
-        transition: transform 0.25s ease-out;
-      }
-
-      &:hover::after {
-        transform: scaleX(1);
-        transform-origin: bottom left;
-      }
-
-      &:active {
-        box-shadow: 0px 0px 0px 0px;
-        top: 5px;
-        left: 5px;
-      }
-    `
-  }}
+      `
+    }}
 `
 
 
