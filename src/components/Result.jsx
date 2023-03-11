0d6a5fcd717b29fd62cb268ee37ea46deb3c9e58
tsumitakaoru <k.tsumita01@gmail.com>
2023-03-11 18:37:39 -0500

    Add style

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 
-const Result = () => {
+const Result = (stat) => {
+
+  console.log(stat);
+
   return (
     <Wrapper>Result</Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-
+  width: 98%;
+  height: 10rem;
+  margin: 0 auto 2rem;
+  border-radius: .5rem;
+  border: 2px solid var(--black-primary);
+  background: var(--tertiary-color);
 `
 
 export default Result
