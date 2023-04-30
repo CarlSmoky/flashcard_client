dfc9def35da38185d3d16aaed76debdfcdceaa6f
tsumitakaoru <k.tsumita01@gmail.com>
2023-04-30 12:48:28 -0400

    Add Edit page

---
+++ src/pages/Edit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Edit = () => {
+  return (
+    <Wrapper>Edit</Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  min-height: calc(100vh - 9.3rem - 9.3rem);
+`
+export default Edit
