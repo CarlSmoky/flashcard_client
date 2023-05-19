31310aab9314887f601e5763b3aeca8ec2df54f2
tsumitakaoru <k.tsumita01@gmail.com>
2023-05-19 11:53:27 -0400

    Format editable data from BE

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { defaultEditableDeck, defaultEditableCard } from '../helpers/defaultEditableData'
 
 const useApplicationData = () => {
 
@@ -8,20 +9,37 @@ const useApplicationData = () => {
   const [flashcardData, setFlashcardData] = useState({});
   const [deckData, setDeckData] = useState({});
 
+  const [editableDeck, setEditableDeck] = useState({ ...defaultEditableDeck });
+  const [editableCards, setEditableCards] = useState([{ ...defaultEditableCard }]);
+
+  //move this function to helper
   const getDeckAndCardsData = async () => {
     try {
       const response = await axios.get(`api/card/deck/${id}`);
       const deckName = response.data.deck.deck_name;
-        const description = response.data.deck.description;
-        setDeckData({deckName, description});
-        const flashcardDataByDeckId = response.data.cards;
-        const formattedCardData = formatFlashcardData(flashcardDataByDeckId);
-        setFlashcardData(formattedCardData);
+      const description = response.data.deck.description;
+      setDeckData({deckName, description});
+      const flashcardDataByDeckId = response.data.cards;
+      console.log(flashcardDataByDeckId);
 
+      const formattedCardData = formatFlashcardData(flashcardDataByDeckId);
+      setFlashcardData(formattedCardData);
+
+
+      // make editableDeck 
+      const formattedEditableDeck = formatEditableDeck(deckName, description)
+      setEditableDeck(formattedEditableDeck);
+
+      // make editableCards
+      const formattedEditableCards = formatEditableCards(flashcardDataByDeckId)
+      setEditableCards(formattedEditableCards)
     } catch (error) {
       console.log(error.response.data.error);
     }
   }
+
+  console.log(editableDeck);
+  console.log(editableCards);
 
   useEffect(() => {
     getDeckAndCardsData();
@@ -43,6 +61,38 @@ const useApplicationData = () => {
         }
       };
     }, initialValue);
+  }
+
+  const formatEditableDeck = (deckName, description) => {
+    return {
+      deckName,
+      description,
+      errors: {
+        deckName: '',
+        description: '',
+      },
+      modifications: {
+        deckName: true,
+        description: true, 
+      }
+    }
+  }
+
+  const formatEditableCards = (flashcardDataByDeckId) => {
+    return flashcardDataByDeckId.map(card => {
+      return {
+        term: card.term,
+        definition: card.definition,
+        errors: {
+          term: '',
+          definition: '',
+        },
+        modifications: {
+          term: true,
+          definition: true,
+        }
+      };
+    })
   }
 
   const setCardProperty = (cardId, property, value) => {
