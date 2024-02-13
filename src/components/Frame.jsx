0ad1518d993bbc9544fd75b53ce426f3db7a5fa9
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-13 12:23:13 -0500

    Remove components

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,15 +1,11 @@
 import styled from 'styled-components'
-import { BsCardText } from 'react-icons/bs'
-// import { BiSearch } from 'react-icons/bi'
-import Nav from './Nav'
+import Navbar from './Navbar'
 import Menu from './Menu'
 import DeckList from '../pages/DeckList'
 import Practice from '../pages/Practice'
 import Edit from '../pages/Edit'
 import Create from '../pages/Create'
 import Footer from './Footer'
-import Signup from './Signup'
-import Login from './Login'
 import ModalProvider from '../providers/ModalProvider'
 
 
@@ -19,21 +15,18 @@ const Frame = ({ content }) => {
     <Wrapper >
       <div className="left">
         <div className="logo">
-          <BsCardText />
         </div>
         <div className="middle"></div>
         <div className="bottom"></div>
       </div>
       <div className="center">
-        <Nav />
+        <Navbar/>
         <ModalProvider>
           {content === "menu" && <Menu />}
           {content === "decklist" && <DeckList />}
           {content === "practice" && <Practice />}
           {content === "edit" && <Edit />}
           {content === "create" && <Create />}
-          {content === "signup" && <Signup />}
-          {content === "login" && <Login />}
         </ModalProvider>
         <Footer />
       </div>
@@ -89,7 +82,7 @@ const Wrapper = styled.div`
   }
 
   .center {
-    width: 90%;
+    width: 100%;
     position: relative;
   }
 
@@ -105,7 +98,6 @@ const Wrapper = styled.div`
       padding: 1rem;
       height: 7rem;
       border-bottom: var(--side-column-border) solid var(--black-primary);
-
     }
     
     .bottom {
@@ -118,19 +110,28 @@ const Wrapper = styled.div`
   @media (max-width: 768px) {
 
     .left {
-      .bottom {
-        padding: 0;
+      .logo {
+        padding: 0rem;
         height: 6rem;
       }
-    }
-    .right {
+
       .bottom {
         padding: 0;
         height: 6rem;
       }
     }
 
-    
+    .right {
+      .search {
+      padding: 0rem;
+      height: 6rem;
+      }
+
+      .bottom {
+        padding: 0;
+        height: 6rem;
+      }
+    }
   }
   
 `
