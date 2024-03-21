fdbdb6997dd6662f404303f478cf67d887f1bb71
tsumitakaoru <k.tsumita01@gmail.com>
2024-03-21 15:43:19 -0400

    Clean code

--- src/components/DeckSettings.jsx
+++ src/components/DeckSettings.jsx
@@ -6,63 +6,6 @@ import Button from './Button'
 import { modes } from '../helpers/modes'
 import { truncate } from '../helpers/utilities';
 
-const DeckSettings = ({
-  setNumCards,
-  deckName,
-  setMode,
-  totalCards
-}) => {
-
-  const [settingNumCards, setsettingNumCards] = useState(0);
-
-  useEffect(() => {
-    setsettingNumCards(totalCards);
-  }, [totalCards]);
-
-  const onChange = (e) => {
-    setsettingNumCards(e.target.value)
-  };
-
-  const handleStart = () => {
-    setNumCards(settingNumCards);
-    setMode(modes.answering);
-  }
-
-  // When cancel button clicked, back to /decklist
-  let navigate = useNavigate();
-
-  return (
-    <Wrapper>
-      <Form>
-        <Header>
-          <button onClick={() => navigate('/decklist')}>
-            <RxCross2 />
-            <span className="visually-hidden">Cancel Button</span>
-          </button>
-        </Header>
-        {deckName && <Title>{truncate(deckName, 18)}</Title>}
-        <Label htmlFor="numCards">Cards: </Label>
-        <input
-          onChange={onChange}
-          id="numCards"
-          type="number"
-          name="numCards"
-          min="1"
-          max={totalCards}
-          value={settingNumCards}
-        />
-        <ButtonWrapper>
-          <Button
-            text="Start"
-            buttonType='button'
-            onButtonClick={handleStart}
-          />
-        </ButtonWrapper>
-      </Form>
-    </Wrapper>
-  )
-}
-
 // Mixin
 const holizontal = () => {
   return `
@@ -153,5 +96,62 @@ const ButtonWrapper = styled.div`
   height: 3rem;
   margin: 4rem 0;
 `
+
+const DeckSettings = ({
+  setNumCards,
+  deckName,
+  setMode,
+  totalCards
+}) => {
+
+  const [settingNumCards, setsettingNumCards] = useState(0);
+
+  useEffect(() => {
+    setsettingNumCards(totalCards);
+  }, [totalCards]);
+
+  const onChange = (e) => {
+    setsettingNumCards(e.target.value)
+  };
+
+  const handleStart = () => {
+    setNumCards(settingNumCards);
+    setMode(modes.answering);
+  }
+
+  // When cancel button clicked, back to /decklist
+  let navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <Form>
+        <Header>
+          <button onClick={() => navigate('/decklist')}>
+            <RxCross2 />
+            <span className="visually-hidden">Cancel Button</span>
+          </button>
+        </Header>
+        {deckName && <Title>{truncate(deckName, 18)}</Title>}
+        <Label htmlFor="numCards">Cards: </Label>
+        <input
+          onChange={onChange}
+          id="numCards"
+          type="number"
+          name="numCards"
+          min="1"
+          max={totalCards}
+          value={settingNumCards}
+        />
+        <ButtonWrapper>
+          <Button
+            text="Start"
+            buttonType='button'
+            onButtonClick={handleStart}
+          />
+        </ButtonWrapper>
+      </Form>
+    </Wrapper>
+  )
+}
 
 export default DeckSettings
