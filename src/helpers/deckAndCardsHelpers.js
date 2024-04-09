8e9fd344dea467f903f92a445090e54e9934ad05
tsumitakaoru <k.tsumita01@gmail.com>
2024-04-09 10:00:30 -0400

    Add AuthenticationGuar

--- src/helpers/deckAndCardsHelpers.js
+++ src/helpers/deckAndCardsHelpers.js
@@ -1,6 +1,9 @@
-import axios from 'axios'
-import { endpoints } from './endpoints'
-import { errorMessage } from '../helpers/messages'
+import axios from "axios";
+import { endpoints } from "./endpoints";
+import { errorMessage } from "../helpers/messages";
+import { callExternalApi } from "./callExternalApi";
+
+const apiServerUrl = process.env.REACT_APP_API_SERVER_URL;
 
 export const getDeckAndCardsDataById = async (id) => {
   try {
@@ -31,22 +34,6 @@ export const updateDeckAndCards = async (updateDeckData, createdCardsData, updat
   }
 };
 
-export const createDeckAndCards = async (newDeckContents, newCardContents) => {
-  const deckContentsForInsertion = getDeckContentsForInsertion(newDeckContents);
-  const cardsContentsForInsertion = getCardsContentsForInsertion(newCardContents)
-  try {
-    const response = await axios.post(endpoints.CREATE_DECK, { newDeckContents: deckContentsForInsertion, newCardContents: cardsContentsForInsertion });
-    return {isUpdated: true,
-      data: response.data
-    };
-  } catch (error) {
-    console.log(error.response.data.error);
-    return {isUpdated: false,
-      error: errorMessage.titleExists
-    };
-  }
-};
-
 const getDeckContentsForInsertion = (newDeckContents) => {
   return {
     deckName: newDeckContents.deckName,
@@ -55,8 +42,31 @@ const getDeckContentsForInsertion = (newDeckContents) => {
 };
 
 const getCardsContentsForInsertion = (newCardContents) => newCardContents.map((card) => {
-    return {
-      term: card.term,
-      definition: card.definition
-    }
+  return {
+    term: card.term,
+    definition: card.definition
+  }
 });
+
+export const postCreateDeckAndCards = async (accessToken, newDeckContents, newCardContents) => {
+  const deckContentsForInsertion = getDeckContentsForInsertion(newDeckContents);
+  const cardsContentsForInsertion = getCardsContentsForInsertion(newCardContents);
+  const config = {
+    url: `${apiServerUrl}/api/deck/create`,
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    data : {
+      newDeckContents: deckContentsForInsertion,
+      newCardContents: cardsContentsForInsertion }
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
