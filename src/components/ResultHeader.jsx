c68a931ee47418de04638f01cd09156789e90827
tsumitakaoru <k.tsumita01@gmail.com>
2024-04-23 10:38:36 -0400

    Fix bug

--- src/components/ResultHeader.jsx
+++ src/components/ResultHeader.jsx
@@ -1,41 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const ResultHeader = ({
-  deckName,
-  numCards,
-  numLearning
-}) => {
-  return (
-    <Wrapper>
-      <div className="row">
-        <h2>{deckName}</h2>
-      <div>
-        <p>Learning: <span>{numLearning}</span></p>
-        <p>Know: <span>{numCards - numLearning}</span></p>
-        <p>Total cards: <span>{numCards}</span></p>
-      </div>
-      </div>
-    </Wrapper>
-  )
-}
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: space-between;
   width: 98%;
   margin: 3rem auto 0;
 
-  .row {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-
   h2 {
     font-size: 1.7rem;
     text-transform: uppercase;
+    word-wrap: break-word;
+    text-align: left;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    min-width: 15rem;
   }
 
   p {
@@ -58,10 +40,33 @@ const Wrapper = styled.div`
     font-size: 1.4rem;
     }
 
+    div {
+    min-width: 13rem;
+    }
+
     p {
       font-size: 1rem;
     }
   }
 `
+
+const ResultHeader = ({
+  deckName,
+  numCards,
+  numLearning
+}) => {
+  return (
+    <Wrapper>
+      <h2>{deckName}</h2>
+      <div>
+        <p>Learning: <span>{numLearning}</span></p>
+        <p>Know: <span>{numCards - numLearning}</span></p>
+        <p>Total cards: <span>{numCards}</span></p>
+      </div>
+    </Wrapper>
+  )
+}
+
+
 
 export default ResultHeader
