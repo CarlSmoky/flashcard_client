8e610dc0375db91ed0c8aa95e5c884abad0b262b
tsumitakaoru <k.tsumita01@gmail.com>
2024-08-22 15:51:29 -0400

    Center Loading component

--- src/components/AuthenticationGuard.jsx
+++ src/components/AuthenticationGuard.jsx
@@ -1,13 +1,22 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import React from "react";
+import styled from "styled-components";
 import LoadingSpinner from "./LoadingSpinner";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  height: 100vh;
+`
 
 export const AuthenticationGuard = ({ component }) => {
   const Component = withAuthenticationRequired(component, {
     onRedirecting: () => (
-      <div className="page-layout">
+      <Container>
         <LoadingSpinner />
-      </div>
+      </Container>
     ),
   });
 
